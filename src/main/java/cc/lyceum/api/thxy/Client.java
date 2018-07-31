8942fa6533c302ac4b499cad08d938a48428fde4
Lyceum <hewun@lyceum.cc>
2018-08-01 06:00:19 +0800

    init
    Signed-off-by: lyceum <hewun@lyceum.cc>

---
+++ src/main/java/cc/lyceum/api/thxy/Client.java
@@ -0,0 +1,31 @@
+package cc.lyceum.api.thxy;
+
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface Client {
+
+    public Response get(String url, Map<String, String> headers) throws IOException;
+
+    public Response post(String url, Map<String, String> headers, Map<String, String> forms) throws IOException;
+
+    default String getBody(String url, Map<String, String> headers) {
+        try {
+            return get(url, headers).body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    default String postBody(String url, Map<String, String> headers, Map<String, String> forms) {
+        try {
+            return post(url, headers, forms).body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/cc/lyceum/api/thxy/JwglClient.java
@@ -0,0 +1,112 @@
+package cc.lyceum.api.thxy;
+
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+public abstract class JwglClient implements Client {
+
+    ConcurrentHashMap<String, List<Cookie>> cookieStore = new ConcurrentHashMap<>();
+
+    @Override
+    public Response get(String url, Map<String, String> headers) throws IOException {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                // cookie
+                .cookieJar(this.newCookieJar())
+                .build();
+
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(url);
+
+        // add header
+        if (headers != null) {
+            headers.forEach(requestBuilder::addHeader);
+        }
+
+        Request request = requestBuilder.build();
+
+        Response response = okHttpClient.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            return response;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Response post(String url, Map<String, String> headers, Map<String, String> forms) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                // cookie
+                .cookieJar(this.newCookieJar())
+                .build();
+
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(url);
+
+        // add header
+        if (headers != null) {
+            headers.forEach(requestBuilder::addHeader);
+        }
+
+        // form
+        if (forms != null) {
+            FormBody.Builder formBody = new FormBody.Builder();
+            forms.forEach(formBody::add);
+            requestBuilder.post(formBody.build());
+        }
+
+        Request request = requestBuilder.build();
+
+        Response response = okHttpClient.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            return response;
+        } else {
+            return null;
+        }
+    }
+
+    CookieJar newCookieJar() {
+        return new CookieJar() {
+            @Override
+            public void saveFromResponse(HttpUrl httpUrl, List<Cookie> list) {
+
+                list = new ArrayList<>(list);
+
+                if (!cookieStore.isEmpty()) {
+                    for (Cookie cookie1 : cookieStore.get(httpUrl.host())) {
+
+                        boolean repeat = false;
+
+                        for (Cookie cookie2 : list) {
+                            if (Objects.equals(cookie1.name(), cookie2.name())) {
+                                repeat = true;
+                                break;
+                            }
+                        }
+
+                        // 如果不重复
+                        if (!repeat) {
+                            list.add(cookie1);
+                        }
+                    }
+                }
+
+                cookieStore.put(httpUrl.host(), list);
+            }
+
+            @Override
+            public List<Cookie> loadForRequest(HttpUrl httpUrl) {
+                List<Cookie> cookies = cookieStore.get(httpUrl.host());
+                return cookies != null ? cookies : new ArrayList<>();
+            }
+        };
+    }
+}
